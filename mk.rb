69c2899b8f8e530f6f55a4dfe23aee092baa72d6
dm <118793435+uname-n@users.noreply.github.com>
2024-11-09 14:13:23 -0800

    mk v2.0.1

--- mk.rb
+++ mk.rb
@@ -1,7 +1,7 @@
 class Mk < Formula
     desc "A lightweight and efficient tool designed to manage and run commands at the project level."
     homepage "https://github.com/uname-n/mk"
-    url "https://github.com/uname-n/mk/archive/refs/tags/v2.0.0.tar.gz"
+    url "https://github.com/uname-n/mk/archive/refs/tags/v2.0.1.tar.gz"
     sha256 "99f5f27fe29934499c06b078edd1f6c7cb9cad5000eaa79d2bbeb3107580a1a5"
     license "GPL-3.0"
   
