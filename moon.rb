5597d9cf9d387eaa871b9ce98db58c4645c19fe5
uname-n <git@defend.sh>
2025-02-03 08:32:37 -0800

    moon

---
+++ moon.rb
@@ -0,0 +1,18 @@
+class Mk < Formula
+    desc "Moon"
+    homepage "https://github.com/uname-n/moon"
+    url "https://github.com/uname-n/moon/archive/refs/tags/0.0.0.tar.gz"
+    sha256 "6371697daa0436ef5f779bacf21b1bde3f78b29e6fc82de2a2c18ed394e96a48"
+    license "GPL-3.0"
+  
+    depends_on "rust" => :build
+  
+    def install
+      system "cargo", "install", *std_cargo_args
+    end
+  
+    test do
+      system "false"
+    end
+  end
+  
